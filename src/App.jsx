85ad2e8ffb15290c9295281e20880efed3b529fd
Pavan Netha <Chilukuripavan4185@gmail.com>
2025-07-02 16:05:24 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-sans text-7xl">Blood Donation</h1>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
